86982bc041f4769bce24d78b3dd6e2c4eeda4db7
Erik Berg <github@slipsprogrammor.no>
2024-09-30 11:20:36 +0200

    nvim: install telescope fuzzy finder

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -27,8 +27,18 @@ vim.g.maplocalleader = "\\"
 
 local plugins = {
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    "nvim-telescope/telescope.nvim", branch = "0.1.x",
+    dependencies = { "nvim-lua/plenary.nvim" }
+  },
 }
 require("lazy").setup(plugins, {})
 
 require("catppuccin").setup()
 vim.cmd.colorscheme "catppuccin"
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
