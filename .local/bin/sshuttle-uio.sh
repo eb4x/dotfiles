99c609307ff9ecdd233e6ca230bd76f2095d0083
Erik Berg <github@slipsprogrammor.no>
2024-10-08 15:43:20 +0200

    sshuttle: conditionally include cidr
    When I'm not on 129.240.0.0/16, add that route.

--- .local/bin/sshuttle-uio.sh
+++ .local/bin/sshuttle-uio.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+CIDRs="172.28.4.0"
+
+if ! [[ $(hostname -I) =~ 129\.240\. ]]; then
+  CIDRs="129.240.0.0/16 $CIDRs"
+fi
+
 sshuttle --daemon \
   --pidfile ${HOME}/sshuttle-uio.pid \
-  --remote login.uio.no 129.240.0.0/16 172.28.4.0/23
+  --remote login.uio.no $CIDRs
