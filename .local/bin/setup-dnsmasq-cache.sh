f06602e14d1c8dc74aec1c87fff185cafb92dad8
Erik Berg <github@slipsprogrammor.no>
2026-05-10 13:46:05 +0200

    dnsmasq/cache: dynamically create entries
    Add Fedora 44, and remove unreliable mirrors. Not that
    fedora.uib.no is reliable. But one unreliable mirror is enough.

--- .local/bin/setup-dnsmasq-cache.sh
+++ .local/bin/setup-dnsmasq-cache.sh
@@ -9,7 +9,7 @@ sudo mkdir -p $tftp_root
 sudo chown -R $USER:$USER $tftp_root
 
 alma_releases=(8 9 10)
-fedora_releases=(42 43 44_Beta)
+fedora_releases=(42 43 44_Beta 44)
 
 # Needs selinux relabeling to allow both dnsmasq and nginx
 sudo semanage fcontext -a -t public_content_t "/var/lib/tftp(/.*)?"
@@ -79,8 +79,8 @@ for releasever in "${fedora_releases[@]}"; do
   mkdir -p $work_dir/images/pxeboot
 
   release_path="${fedora_release_paths[$releasever]:-$releasever}"
-  #http_root=https://fedora.uib.no/fedora/linux/releases/${release_path}/Everything/x86_64/os
-  http_root=https://download.fedoraproject.org/pub/fedora/linux/releases/${release_path}/Everything/x86_64/os
+  http_root=https://fedora.uib.no/fedora/linux/releases/${release_path}/Everything/x86_64/os
+  #http_root=https://download.fedoraproject.org/pub/fedora/linux/releases/${release_path}/Everything/x86_64/os
   for pxe_file in images/install.img images/pxeboot/initrd.img images/pxeboot/vmlinuz; do
     if [ ! -f $work_dir/$pxe_file ]; then
       download_file "$http_root/$pxe_file" "$work_dir/$pxe_file"
@@ -98,46 +98,52 @@ tee /var/lib/tftp/cached.ipxe > /dev/null << EOF
 #!ipxe
 
 set fedora-mirror-url https://fedora.uib.no/fedora/linux/releases
-#set fedora-mirror-url https://mirror.accum.se/mirror/fedora/linux/releases
-#set fedora-mirror-url https://mirrors.dotsrc.org/fedora-enchilada/linux/releases
-#set fedora-mirror-url https://www.nic.funet.fi/pub/mirrors/fedora.redhat.com/pub/fedora/linux/releases
+#set fedora-mirror-url https://download.fedoraproject.org/pub/fedora/linux/releases
 
 menu Cached installers
-item almalinux-10 AlmaLinux 10
-item almalinux-9 AlmaLinux 9
-item almalinux-8 AlmaLinux 8
-item fedora-42   Fedora 42
-item fedora-41   Fedora 41
-item gparted     GParted
+EOF
+
+# menu items
+for releasever in "${alma_releases[@]}"; do
+  echo "item almalinux-${releasever,,} AlmaLinux ${releasever}" >> /var/lib/tftp/cached.ipxe
+done
+for releasever in "${fedora_releases[@]}"; do
+  echo "item fedora-${releasever,,} Fedora ${releasever}" >> /var/lib/tftp/cached.ipxe
+done
+echo "item gparted GParted" >> /var/lib/tftp/cached.ipxe
+
+tee -a /var/lib/tftp/cached.ipxe > /dev/null << EOF
 choose os
 goto \${os}
+EOF
 
-:almalinux-10
-kernel http://\${next-server}:8000/almalinux/10/x86_64/os/images/pxeboot/vmlinuz ip=dhcp inst.stage2=http://\${next-server}:8000/almalinux/10/x86_64/os inst.repo=https://almalinux.uib.no/10/BaseOS/x86_64/os inst.ks=https://raw.githubusercontent.com/eb4x/dotfiles/refs/heads/main/.local/share/kickstart/almalinux-9.ks
-initrd http://\${next-server}:8000/almalinux/10/x86_64/os/images/pxeboot/initrd.img
-boot
+append_alma_stanza() {
+  local releasever=$1
+  cat >> /var/lib/tftp/cached.ipxe << EOF
 
-:almalinux-9
-kernel http://\${next-server}:8000/almalinux/9/x86_64/os/images/pxeboot/vmlinuz ip=dhcp inst.stage2=http://\${next-server}:8000/almalinux/9/x86_64/os inst.repo=https://almalinux.uib.no/9/BaseOS/x86_64/os inst.ks=https://raw.githubusercontent.com/eb4x/dotfiles/refs/heads/main/.local/share/kickstart/almalinux-9.ks
-initrd http://\${next-server}:8000/almalinux/9/x86_64/os/images/pxeboot/initrd.img
+:almalinux-${releasever}
+kernel http://\${next-server}:8000/almalinux/${releasever}/x86_64/os/images/pxeboot/vmlinuz ip=dhcp inst.stage2=http://\${next-server}:8000/almalinux/${releasever}/x86_64/os inst.repo=https://almalinux.uib.no/${releasever}/BaseOS/x86_64/os inst.ks=https://raw.githubusercontent.com/eb4x/dotfiles/refs/heads/main/.local/share/kickstart/almalinux-9.ks
+initrd http://\${next-server}:8000/almalinux/${releasever}/x86_64/os/images/pxeboot/initrd.img
 boot
+EOF
+}
+append_fedora_stanza() {
+  local releasever=$1
+  local release_path="${fedora_release_paths[$releasever]:-$releasever}"
+  cat >> /var/lib/tftp/cached.ipxe << EOF
 
-:almalinux-8
-kernel http://\${next-server}:8000/almalinux/8/x86_64/os/images/pxeboot/vmlinuz ip=dhcp inst.stage2=http://\${next-server}:8000/almalinux/8/x86_64/os inst.repo=https://almalinux.uib.no/8/BaseOS/x86_64/os inst.ks=https://raw.githubusercontent.com/eb4x/dotfiles/refs/heads/main/.local/share/kickstart/almalinux-9.ks
-initrd http://\${next-server}:8000/almalinux/8/x86_64/os/images/pxeboot/initrd.img
+:fedora-${releasever,,}
+set fedora-inst-repo-url \${fedora-mirror-url}/${release_path}/Everything/\${arch}/os
+kernel http://\${next-server}:8000/fedora/${releasever}/x86_64/os/images/pxeboot/vmlinuz ip=dhcp inst.stage2=http://\${next-server}:8000/fedora/${releasever}/x86_64/os inst.repo=\${fedora-inst-repo-url} inst.ks=https://raw.githubusercontent.com/eb4x/dotfiles/refs/heads/main/.local/share/kickstart/fedora.ks
+initrd http://\${next-server}:8000/fedora/${releasever}/x86_64/os/images/pxeboot/initrd.img
 boot
+EOF
+}
 
-:fedora-42
-set fedora-inst-repo-url \${fedora-mirror-url}/42/Everything/\${arch}/os
-kernel http://\${next-server}:8000/fedora/42/x86_64/os/images/vmlinuz ip=dhcp inst.stage2=http://\${next-server}:8000/fedora/41/x86_64/os inst.repo=\${fedora-inst-repo-url} inst.ks=https://raw.githubusercontent.com/eb4x/dotfiles/refs/heads/main/.local/share/kickstart/fedora.ks
-initrd http://\${next-server}:8000/fedora/42/x86_64/os/images/initrd.img
-boot
+for releasever in "${alma_releases[@]}"; do append_alma_stanza "$releasever"; done
+for releasever in "${fedora_releases[@]}"; do append_fedora_stanza "$releasever"; done
 
-:fedora-41
-set fedora-inst-repo-url \${fedora-mirror-url}/41/Everything/\${arch}/os
-kernel http://\${next-server}:8000/fedora/41/x86_64/os/images/vmlinuz ip=dhcp inst.stage2=http://\${next-server}:8000/fedora/41/x86_64/os inst.repo=\${fedora-inst-repo-url} inst.ks=https://raw.githubusercontent.com/eb4x/dotfiles/refs/heads/main/.local/share/kickstart/fedora.ks
-initrd http://\${next-server}:8000/fedora/41/x86_64/os/images/initrd.img
-boot
+tee -a /var/lib/tftp/cached.ipxe > /dev/null << EOF
 
 :gparted
 kernel http://\${next-server}:8000/gparted/${gparted_version}/live/vmlinuz boot=live config components union=overlay username=user noswap noeject vga=788 fetch=http://\${next-server}:8000/gparted/${gparted_version}/live/filesystem.squashfs
